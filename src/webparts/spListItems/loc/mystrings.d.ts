d46200d57aecc2cb0a37d2a284fbdaf58e7a24b4
26-hARSha <harshadasannake@outlook.com>
2023-06-07 00:06:21 +0530

    Demo 1

---
+++ src/webparts/spListItems/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ISpListItemsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'SpListItemsWebPartStrings' {
+  const strings: ISpListItemsWebPartStrings;
+  export = strings;
+}
